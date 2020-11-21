bea6b5472c4f943b3de6dc7fa64d46ae1669ced5
jiho lee <jiholee@kpu.ac.kr>
2020-11-21 15:40:31 +0900

    Fix views

--- DjangoCRUDBoard/urls.py
+++ DjangoCRUDBoard/urls.py
@@ -15,8 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from board import views
 
 urlpatterns = [
-    path('', admin.site.urls),
+    path('', views.index),
     path('admin/', admin.site.urls),
 ]
