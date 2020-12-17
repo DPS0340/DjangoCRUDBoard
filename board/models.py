c6fcba949b91fb8aaf75118ae0ae0573f656e82a
jiho lee <jiholee@kpu.ac.kr>
2020-12-17 14:29:31 +0900

    Add models

--- board/models.py
+++ board/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+import django.contrib.auth.models as authModels
 
 # Create your models here.
+
+class User(authModels.User):
+    pass
+
+class Board(models.Model):
+    name = models.CharField()
+
+class Post(models.Model):
+    board = models.ForeignKey(Board, on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField()
+    content = models.TextField()
+
+class Reply(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField()
+
+class AnswerReply(models.Model):
+    reply = models.ForeignKey(Reply, on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField()
