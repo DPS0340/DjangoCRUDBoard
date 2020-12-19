2fef35ca9efdb676193d164dc80e3a6825217228
jiho lee <jiholee@kpu.ac.kr>
2020-12-19 19:36:51 +0900

    Add pop_args

--- board/utils.py
+++ board/utils.py
@@ -2,6 +2,10 @@ from django.http import HttpResponse
 import json
 
 
-def sendJson(data):
+def send_json(data):
     res = json.dumps(data)
     return HttpResponse(res, content_type='application/json')
+
+
+def pop_args(dict, *args):
+    return {arg: dict[arg] for arg in args}
