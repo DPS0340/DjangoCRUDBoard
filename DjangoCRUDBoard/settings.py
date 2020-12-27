bbe03dd4f6a17c8eb59cd32f688ef60b9fb42fee
jiho lee <jiholee@kpu.ac.kr>
2020-12-27 12:48:26 +0900

    Remove database settings

--- DjangoCRUDBoard/settings.py
+++ DjangoCRUDBoard/settings.py
@@ -82,12 +82,6 @@ WSGI_APPLICATION = 'DjangoCRUDBoard.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
 
 
 # Password validation
