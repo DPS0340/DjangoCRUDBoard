c4f85309cad9ce599c2bbe82678914f70e863290
jiho lee <jiholee@kpu.ac.kr>
2020-12-27 17:50:32 +0900

    Add null check

--- board/views/login.py
+++ board/views/login.py
@@ -8,6 +8,8 @@ class LoginView(View):
     def post(self, request):
         if 'userid' in request.session:
             return send_json(userAlreadyLogin)
+        if 'username' not in request.POST or 'password' not in request.POST:
+            return send_json(illegalArgument)
         user = authenticate(username=request.POST['username'], password=request.POST['password'])
         if user is not None:
             request.session['userid'] = user.id
