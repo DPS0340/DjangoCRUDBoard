5098e56513a32acafa2a95bd25f68c8634772022
jiho lee <jiholee@kpu.ac.kr>
2020-12-27 13:12:16 +0900

    Fix DB url

--- DjangoCRUDBoard/settings.py
+++ DjangoCRUDBoard/settings.py
@@ -85,7 +85,7 @@ WSGI_APPLICATION = 'DjangoCRUDBoard.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR + '/db.sqlite3'
+        'NAME': BASE_DIR / 'db.sqlite3'
     }
 }
 
