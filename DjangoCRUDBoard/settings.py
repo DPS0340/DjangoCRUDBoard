7d3a0cfb0c1307a62769913b3a38974992681bb8
jiho lee <jiholee@kpu.ac.kr>
2020-12-27 13:11:41 +0900

    Fix DB

--- DjangoCRUDBoard/settings.py
+++ DjangoCRUDBoard/settings.py
@@ -82,6 +82,12 @@ WSGI_APPLICATION = 'DjangoCRUDBoard.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR + '/db.sqlite3'
+    }
+}
 
 
 # Password validation
