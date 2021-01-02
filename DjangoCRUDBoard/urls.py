a8492450ef35e4c38dc8b4b73bb73330c0586a11
jiho lee <jiholee@kpu.ac.kr>
2021-01-03 01:16:11 +0900

    Fix post url with/out slash

--- DjangoCRUDBoard/urls.py
+++ DjangoCRUDBoard/urls.py
@@ -1,20 +1,5 @@
-"""DjangoCRUDBoard URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
-from django.urls import path
+from django.urls import re_path as path
 from board.views.index import IndexView
 from board.views.register import RegisterView
 from board.views.delete_user import DeleteUserView
@@ -26,17 +11,16 @@ from board.views.status import StatusView
 import sys
 
 sys.path.append("../board")
-from board.decorators import login_required
 
 urlpatterns = [
-    path('', IndexView.as_view()),
-    path('admin/', admin.site.urls),
-    path('register/', RegisterView.as_view()),
-    path('delete_user/', DeleteUserView.as_view()),
-    path('login/', LoginView.as_view()),
-    path('logout/', LogoutView.as_view()),
-    path('post/', PostView.as_view()),
-    path('board/', BoardView.as_view()),
-    path('status/', StatusView.as_view()),
+    path(r'^/?$', IndexView.as_view()),
+    path(r'^admin/?$', admin.site.urls),
+    path(r'^register/?$', RegisterView.as_view()),
+    path(r'^delete_user/?$', DeleteUserView.as_view()),
+    path(r'^login/?$', LoginView.as_view()),
+    path(r'^logout/?$', LogoutView.as_view()),
+    path(r'^post/?$', PostView.as_view()),
+    path(r'^board/?$', BoardView.as_view()),
+    path(r'^status/?$', StatusView.as_view()),
 
 ]
