935a592a80a7de60db9dd7a02ec2905c7fda65c3
jiho lee <jiholee@kpu.ac.kr>
2021-01-05 02:57:00 +0900

    make secret key case insensitive

--- DjangoCRUDBoard/secret_key.py
+++ DjangoCRUDBoard/secret_key.py
@@ -1,3 +1,3 @@
 import os
 
-SECRET_KEY = os.environ['Django_secret_key']
+SECRET_KEY = os.environ['Django_secret_key'] if 'Django_secret_key' in os.environ else os.environ['Django_secret_key'.upper()]
