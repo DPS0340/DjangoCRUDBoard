5385f85bab5d814683431ec92c78da727f25d120
jiho lee <optional.int@kakao.com>
2021-01-05 03:00:01 +0900

    Revert "Merge pull request #27 from DPS0340/FIx-improved-environ-handling"
    This reverts commit 7c00ad8c0165d706be6a11ed95dca070298a468c.

--- DjangoCRUDBoard/secret_key.py
+++ DjangoCRUDBoard/secret_key.py
@@ -1,3 +1,3 @@
 import os
 
-SECRET_KEY = os.environ['Django_secret_key'] if 'Django_secret_key' in os.environ else os.environ['Django_secret_key'.upper()]
+SECRET_KEY = os.environ['Django_secret_key']
