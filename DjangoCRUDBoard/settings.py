5064a8c4df861e5a949bfaa71b0159cfad3a9d34
jiho lee <jiholee@kpu.ac.kr>
2021-01-06 02:02:28 +0900

    Update settings.py

--- DjangoCRUDBoard/settings.py
+++ DjangoCRUDBoard/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
-# from DjangoCRUDBoard import secret_key
+from DjangoCRUDBoard import secret_key
 from DjangoCRUDBoard.disable import DisableCSRF
 from pathlib import Path
 import os
