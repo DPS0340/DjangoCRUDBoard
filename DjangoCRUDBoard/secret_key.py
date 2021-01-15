c9670b5152364cd00befefa67a25a9cc143c29f4
qkr0677 <qkr0677@gmail.com>
2021-01-15 20:57:10 +0900

    Implemented answer-reply #4

--- DjangoCRUDBoard/secret_key.py
+++ DjangoCRUDBoard/secret_key.py
@@ -1,3 +1,3 @@
 import os
 
-SECRET_KEY = os.environ['Django_secret_key']
+SECRET_KEY = '&6tlpy9pfc@e2uf%_m=(h!(#5^z8ze46hsp6jklf0$w*ds6-q-'
--- board/views/answer_reply.py
+++ board/views/answer_reply.py
@@ -1,47 +1,49 @@
 from ..responses import *
 from ..utils import send_json, pop_args
-from ..models import User, Reply, AnswerReply
+from ..models import Post, User, Reply, AnswerReply
 from ..decorators import login_required
 from ..utils import decode_jwt
 from django.views import View
 from django.core.serializers import serialize
 import json
 
+# 대댓글 TODO
 
-# 댓글 TODO
-
-# post title -> ID 변경 TODO
 
 class AnswerReplyView(View):
     def get(self, request):
-        if "pk" not in request.GET:
+        if "pk" not in request.GET:  # post.py의 primary key를 받지 못하면 replyRequired 응답
             return send_json(replyRequired)
+        # answer_reply 테이블을 저장? url받는 건가?
         data = getSucceedFunc('answer_reply')
+
         try:
-            reply = Reply.objects.get(pk=request.GET["pk"])
-        except Reply.DoesNotExist:
+            reply = Reply.objects.get(
+                pk=request.GET['pk'])  # Reply 테이블의 pk를 받는거?
+        except Reply.DoesNotExist:           # Reply 필드 데이터 없으면
             return send_json(replyDoesNotExists)
-        replys = json.loads(
+        answerreplys = json.loads(
             serialize(
                 "json",
                 AnswerReply.objects.filter(reply=reply)
-                .order_by('id') # 대댓글은 오름차순
+                .order_by('id')  # 대댓글 오름차순
             )
         )
-        data['data'] = replys
+        data['data'] = answerreplys  # 'data' 키에 들어가는 값의 자료형이 딕션?
         return send_json(data)
 
-    @login_required # 로그인 된 사람만 댓글을 쓸 수 있음
+    @login_required  # 로그인 된 사람만 댓글을 쓸 수 있음
     def post(self, request):
         dic = pop_args(request.POST, "pk", "content")
         # 하나라도 없다면 illegalArgument 처리
         if None in dic.values():
             return send_json(illegalArgument)
-        session = request.session
-        decoded = decode_jwt(session)
+        session = request.session      # 접속 유저중 특정 유저를 인식을 위함?
+        decoded = decode_jwt(session)  # 암호화 해독
         userid = decoded['userid']
-        reply = Reply.objects.filter(pk=dic['pk'])[0] 
+        reply = Reply.objects.filter(pk=dic['pk'])[0]
         author = User.objects.filter(id=userid)[0]
-        AnswerReply.objects.create(reply=reply, author=author, content=dic['content'])
-        data = replySucceed
+        AnswerReply.objects.create(reply=reply, author=author,
+                                   content=dic['content'])
+        data = answerReplySucceed
         return send_json(data)
