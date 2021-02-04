7d8008804ab7693f97d4065dc1d0a22b84b84a23
qkr0677 <qkr0677@gmail.com>
2021-02-04 20:07:26 +0900

    #38 feature-add-answerreply-delete

--- board/views/answer_reply.py
+++ board/views/answer_reply.py
@@ -5,21 +5,36 @@ from ..decorators import login_required
 from ..utils import decode_jwt
 from django.views import View
 from django.core.serializers import serialize
+from urllib import parse
 import json
 
 # 대댓글 TODO
+
+
+def byte_to_dict(data):
+    body_unicode = data.decode('utf-8')
+    body = body_unicode.replace("&", "=")
+    body_list = body.split("=")
+    body_key = []
+    body_value = []
+    for i in range(len(body_list)):
+        if i % 2 == 0:
+            body_key.append(body_list[i])
+        else:
+            body_value.append(parse.unquote(body_list[i]))
+    dict_body = dict(zip(body_key, body_value))
+    return dict_body
 
 
 class AnswerReplyView(View):
     def get(self, request):
         if "pk" not in request.GET:  # post.py의 primary key를 받지 못하면 replyRequired 응답
             return send_json(replyRequired)
-        # answer_reply 테이블을 저장? url받는 건가?
         data = getSucceedFunc('answer_reply')
 
         try:
             reply = Reply.objects.get(
-                pk=request.GET['pk'])  # Reply 테이블의 pk를 받는거?
+                pk=request.GET['pk'])
         except Reply.DoesNotExist:           # Reply 필드 데이터 없으면
             return send_json(replyDoesNotExists)
         answerreplys = json.loads(
@@ -29,7 +44,7 @@ class AnswerReplyView(View):
                 .order_by('id')  # 대댓글 오름차순
             )
         )
-        data['data'] = answerreplys  # 'data' 키에 들어가는 값의 자료형이 딕션?
+        data['data'] = answerreplys
         return send_json(data)
 
     @login_required  # 로그인 된 사람만 댓글을 쓸 수 있음
@@ -38,7 +53,7 @@ class AnswerReplyView(View):
         # 하나라도 없다면 illegalArgument 처리
         if None in dic.values():
             return send_json(illegalArgument)
-        session = request.session      # 접속 유저중 특정 유저를 인식을 위함?
+        session = request.session      # 접속 유저중 특정 유저를 인식
         decoded = decode_jwt(session)  # 암호화 해독
         userid = decoded['userid']
         reply = Reply.objects.filter(pk=dic['pk'])[0]
@@ -47,3 +62,20 @@ class AnswerReplyView(View):
                                    content=dic['content'])
         data = answerReplySucceed
         return send_json(data)
+
+    @login_required
+    def delete(self, request):
+        if 'pk' not in byte_to_dict(request.body):
+            return send_json(illegalArgument)
+        else:
+            user_pk = byte_to_dict(request.body)  # 대댓글 pk
+            answer = AnswerReply.objects.get(pk=user_pk['pk'])
+        session = request.session      # 접속 유저중 특정 유저를 인식
+        decoded = decode_jwt(session)
+        userid = decoded['userid']  # 로그인한 유저의 pk
+
+        if userid == answer.author.id:  # 로그인한 유저와 삭제할 대댓글 작성 유저가 같으면
+            answer.delete()
+            return send_json(deleteAnsreplySucceed)
+        else:
+            return send_json(AnsDoesNotMatch)
