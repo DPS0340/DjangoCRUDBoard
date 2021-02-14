e79241f0ee97847bad164867d06aea8421d03f1b
jiho lee <optional.int@kakao.com>
2021-02-14 19:41:25 +0900

    add log function

--- DjangoCRUDBoard/settings.py
+++ DjangoCRUDBoard/settings.py
@@ -51,6 +51,26 @@ INSTALLED_APPS = [
     'corsheaders'
 ]
 
+if DEBUG == False:
+    LOGGING = {
+        'version': 1,
+        'disable_existing_loggers': False,
+        'handlers': {
+            'file': {
+                'level': 'DEBUG',
+                'class': 'logging.FileHandler',
+                'filename': '/var/log/app-logs/django.log',
+            },
+        },
+        'loggers': {
+            'django': {
+                'handlers': ['file'],
+                'level': 'DEBUG',
+                'propagate': True,
+            },
+        },
+    }
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
