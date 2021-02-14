6d5c422e4df13d3a91e1b79482b74db4836f73c0
jiho lee <optional.int@kakao.com>
2021-02-14 23:54:17 +0900

    delete migrations folder

---
+++ query.py
@@ -0,0 +1,8 @@
+import psycopg2
+
+import os
+
+conn = psycopg2.connect(host=os.environ['DB_HOST'], port=os.environ['DB_PORT'], user=os.environ['DB_USER'], password=os.environ['DB_PASSWORD'])
+cur = conn.cursor()
+# cur.execute("DROP TABLE board_post CASCADE;")
+cur.execute("select 'drop table "' || tablename || '" cascade;' from pg_tables;")
