66c0481020fb576f7bc9f65e3e38aa3d6e1ae01e
SeungWookHan <hswook12@me.com>
2021-02-18 13:27:39 +0900

    Add: Dockerize feature
    Co-authored-by: DPS0340 <optional.int@kakao.com>
    Co-authored-by: Gnu-Kenny <qkr0677@gmail.com>

--- DjangoCRUDBoard/settings.py
+++ DjangoCRUDBoard/settings.py
@@ -52,24 +52,25 @@ INSTALLED_APPS = [
 ]
 
 if DEBUG == False:
-    LOGGING = {
-        'version': 1,
-        'disable_existing_loggers': False,
-        'handlers': {
-            'file': {
-                'level': 'DEBUG',
-                'class': 'logging.FileHandler',
-                'filename': '/var/log/app-logs/django.log',
-            },
-        },
-        'loggers': {
-            'django': {
-                'handlers': ['file'],
-                'level': 'DEBUG',
-                'propagate': True,
-            },
-        },
-    }
+    pass
+    # LOGGING = {
+    #     'version': 1,
+    #     'disable_existing_loggers': False,
+    #     'handlers': {
+    #         'file': {
+    #             'level': 'DEBUG',
+    #             'class': 'logging.FileHandler',
+    #             'filename': '/var/log/app-logs/django.log',
+    #         },
+    #     },
+    #     'loggers': {
+    #         'django': {
+    #             'handlers': ['file'],
+    #             'level': 'DEBUG',
+    #             'propagate': True,
+    #         },
+    #     },
+    # }
 
 
 MIDDLEWARE = [
--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,35 @@
 FROM python:3.8
 ENV PYTHONUNBUFFERED 1
-WORKDIR /code                
-RUN pip install pipenv
-COPY Pipfile Pipfile.lock ./  
-RUN pipenv install
-COPY . ./
-EXPOSE 80                   
 
-CMD python manage.py runserver 0.0.0.0:80
+RUN apt-get -y update 
+
+ENV Django_secret_key abcde0s&&$uyc)hf_3rv@!a95nasd22e-dxt^9k^7!f+$jxkk+$k-
+ENV BOARD_DEBUG 1
+
+# 유저, 그룹 나중에 수정 TODO
+# the user to run as
+ENV USER root
+
+# how many worker processes should Gunicorn spawn
+ENV NUM_WORKERS 3
+
+# which settings file should Django use
+ENV DJANGO_SETTINGS_MODULE DjangoCRUDBoard.settings
+
+# WSGI module name
+ENV DJANGO_WSGI_MODULE DjangoCRUDBoard.wsgi
+
+ENV PORT 8000
+
+RUN echo "Starting $NAME as `whoami`"
+
+WORKDIR /code
+ADD . /code              
+
+RUN pip install --upgrade pip
+RUN pip install pipenv
+RUN pipenv install
+
+EXPOSE 8000                   
+
+ENTRYPOINT ["./bin/gunicorn_start"]
