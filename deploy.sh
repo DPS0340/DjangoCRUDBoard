e94b79a4c38b735143cf2072a49ba878737c7ec5
jiho lee <optional.int@kakao.com>
2021-02-19 23:51:31 +0900

    Fix: codedeploy path && deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,1 +1,7 @@
-docker-compose down && docker-compose up --build
+#!/bin/sh
+
+REPOSITORY=/home/ec2-user/app
+
+cd $REPOSITORY
+
+nohup docker-compose down && docker-compose up --build > $REPOSITORY/nohup.out 2>&1 &
