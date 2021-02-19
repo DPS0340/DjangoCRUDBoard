19b68756d2c553acfe2f2e8aa0ce2705e3f646cc
jiho lee <optional.int@kakao.com>
2021-02-20 02:58:01 +0900

    Add: docker-compose install script

--- deploy.sh
+++ deploy.sh
@@ -4,4 +4,7 @@ REPOSITORY=/home/ubuntu/app
 
 cd $REPOSITORY
 
+sudo apt-get update \
+  && sudo DEBIAN_FRONTEND=noninteractive apt-get install -y docker-compose
+
 nohup docker-compose down && docker-compose up --build > $REPOSITORY/nohup.out 2>&1 &
