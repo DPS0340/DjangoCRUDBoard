949ac3bd57183433775c1a916bc1705fcbf44ab3
jiho lee <optional.int@kakao.com>
2021-02-21 00:53:00 +0900

    Improve: error handling

--- afterinstall.sh
+++ afterinstall.sh
@@ -1,6 +1,11 @@
 
 if [ -d /home/ubuntu/app-backup/ ]; then
     mkdir -p /home/ubuntu/app
-    mv /home/ubuntu/app-backup/data /home/ubuntu/app/data
-    mv /home/ubuntu/app-backup/nginx/logs /home/ubuntu/app/nginx/logs
+    if [ -d /home/ubuntu/app-backup/data ]; then
+        mv /home/ubuntu/app-backup/data /home/ubuntu/app/data
+    fi
+    if [ -d /home/ubuntu/app-backup/logs ]; then
+        mkdir -p /home/ubuntu/app/nginx
+        mv /home/ubuntu/app-backup/nginx/logs /home/ubuntu/app/nginx/logs
+    fi
 fi
