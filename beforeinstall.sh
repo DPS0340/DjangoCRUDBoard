5bf0e3c6f8e1bf0f0aec4efd736359882f25d2e1
jiho lee <optional.int@kakao.com>
2021-02-21 00:17:15 +0900

    Fix: overwrite not working issue
    check https://github.com/aws/aws-codedeploy-agent/issues/14

---
+++ beforeinstall.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+# https://github.com/aws/aws-codedeploy-agent/issues/14 이슈 참조
+if [ -d /home/ubuntu/app/ ]; then
+    if [ -d /home/ubuntu/app-backup ]; then
+        rm -rf /home/ubuntu/app-backup
+    fi
+    mkdir -p /home/ubuntu/app-backup
+    mv /home/ubuntu/app /home/ubuntu/app-backup
+fi
+mkdir -p /home/ubuntu/app
