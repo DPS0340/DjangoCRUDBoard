739a65c24e7e8b748b7c3753095c7e47beb7abf4
jiho lee <optional.int@kakao.com>
2021-02-21 22:40:13 +0900

    execute init-letsencrypt.sh on deploy

--- deploy.sh
+++ deploy.sh
@@ -9,6 +9,8 @@ cd $REPOSITORY
 sudo apt-get update \
   && sudo DEBIAN_FRONTEND=noninteractive apt-get install -y docker-compose
 
+./init-letsencrypt.sh
+
 sudo chmod -R 777 data
 
 mkdir -p data/db
