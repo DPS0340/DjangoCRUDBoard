a5a57bb191cb36d46b98a4620ca02f63be99ac51
jiho lee <optional.int@kakao.com>
2021-02-22 00:58:29 +0900

    delete: www domain

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -5,7 +5,7 @@ if ! [ -x "$(command -v docker-compose)" ]; then
   exit 1
 fi
 
-domains=(djangocrudboard.ga www.djangocrudboard.ga)
+domains=(djangocrudboard.ga)
 rsa_key_size=4096
 data_path="./data/certbot"
 email="optional.int@kakao.com" # Adding a valid address is strongly recommended
