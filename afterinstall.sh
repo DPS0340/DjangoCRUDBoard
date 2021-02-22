5a06c21635028fe8103f63d20ca866d91bb00488
DPS0340 <optional.int@kakao.com>
2021-02-23 01:56:18 +0900

    some fix

--- afterinstall.sh
+++ afterinstall.sh
@@ -3,11 +3,11 @@ if [ -d /home/ubuntu/app-backup/ ]; then
     mkdir -p /home/ubuntu/app
     if [ -d /home/ubuntu/app-backup/data ]; then
         mkdir -p /home/ubuntu/app/data
-        mv /home/ubuntu/app-backup/data/* /home/ubuntu/app/data
+        yes | cp -arpf /home/ubuntu/app-backup/data/* /home/ubuntu/app/data
     fi
     if [ -d /home/ubuntu/app-backup/logs ]; then
         mkdir -p /home/ubuntu/app/nginx
-        mv /home/ubuntu/app-backup/nginx/logs /home/ubuntu/app/nginx/logs
+        yes | cp -arpf /home/ubuntu/app-backup/nginx/logs /home/ubuntu/app/nginx/logs
     fi
 fi
 cp /home/ubuntu/app/nginx/nginx-release.conf /home/ubuntu/app/nginx/nginx-app.conf
