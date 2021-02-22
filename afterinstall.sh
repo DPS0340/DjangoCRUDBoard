8b297aafd8c9df2a3cb10e709d8a804db9809754
jiho lee <optional.int@kakao.com>
2021-02-22 14:16:01 +0900

    Separated: debug and release nginx config

--- afterinstall.sh
+++ afterinstall.sh
@@ -9,3 +9,4 @@ if [ -d /home/ubuntu/app-backup/ ]; then
         mv /home/ubuntu/app-backup/nginx/logs /home/ubuntu/app/nginx/logs
     fi
 fi
+cp /home/ubuntu/app/nginx/nginx-release.conf /home/ubuntu/app/nginx/nginx-app.confs
