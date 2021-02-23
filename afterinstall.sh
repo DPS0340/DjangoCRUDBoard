041957711752594364aba6e0b42718a9a51df421
DPS0340 <optional.int@kakao.com>
2021-02-23 22:47:50 +0900

    Fix: nginx after install path

--- afterinstall.sh
+++ afterinstall.sh
@@ -5,9 +5,9 @@ if [ -d /home/ubuntu/app-backup/ ]; then
         mkdir -p /home/ubuntu/app/data
         yes | cp -arpf /home/ubuntu/app-backup/data/* /home/ubuntu/app/data
     fi
-    if [ -d /home/ubuntu/app-backup/logs ]; then
-        mkdir -p /home/ubuntu/app/nginx
-        yes | cp -arpf /home/ubuntu/app-backup/nginx/logs /home/ubuntu/app/nginx/logs
+    if [ -d /home/ubuntu/app-backup/nginx/logs ]; then
+        mkdir -p /home/ubuntu/app/nginx/logs
+        yes | cp -arpf /home/ubuntu/app-backup/nginx/logs/* /home/ubuntu/app/nginx/logs
     fi
 fi
 cp /home/ubuntu/app/nginx/nginx-release.conf /home/ubuntu/app/nginx/nginx-app.conf
