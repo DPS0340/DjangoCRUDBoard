2f52482bd5662fbb358ef6b1bc44ac47e9186371
ww8007 <wshmin@naver.com>
2021-02-24 02:50:24 +0900

    Adding files

--- DjangoCRUDBoard/secret_key.py
+++ DjangoCRUDBoard/secret_key.py
@@ -1,3 +1,4 @@
 import os
 
-SECRET_KEY = os.environ['Django_secret_key']
+# SECRET_KEY = os.environ['ve#bjgcq@bi7c*zzj1^_@=^42$)1a-gjs#i!(h4_gq_s$#h&g&']
+SECRET_KEY = "ve#bjgcq@bi7c*zzj1^_@=^42$)1a-gjs#i!(h4_gq_s$#h&g&"
