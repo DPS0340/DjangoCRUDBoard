1ddb47f83f26ad5d115135eeed59e8b7d541f269
jiho lee <optional.int@kakao.com>
2021-02-26 16:26:16 +0900

    Revert "Update deploy.sh"
    This reverts commit f684065b577e0520661d5a4eada30b1edfabb151.

--- deploy.sh
+++ deploy.sh
@@ -19,6 +19,6 @@ sudo chmod -R 777 $REPOSITORY/data
 
 sudo chown -R ubuntu $REPOSITORY/data
 
-touch $REPOSITORY/nohup.out
 
-nohup docker-compose up --build > $REPOSITORY/nohup.out 2>&1 &
+docker-compose down -v > $REPOSITORY/nohup.out
+nohup docker-compose up --build >> $REPOSITORY/nohup.out 2>&1 &
