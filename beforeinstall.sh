cddff139201d9a56f8805a10bd185e98214740dd
jiho lee <optional.int@kakao.com>
2021-03-11 17:12:55 +0900

    Update beforeinstall.sh

--- beforeinstall.sh
+++ beforeinstall.sh
@@ -2,12 +2,12 @@
 
 # https://github.com/aws/aws-codedeploy-agent/issues/14 이슈 참조
 if [ -d /home/ubuntu/app/ ]; then
-    if [ -d /home/ubuntu/app-backup ]; then
-        rm -rf /home/ubuntu/app-backup
-    fi
     if [ -f /home/ubuntu/app/docker-compose.yml ]; then
         cd /home/ubuntu/app
-        docker-compose down
+        sudo docker-compose down -v
+    fi
+    if [ -d /home/ubuntu/app-backup ]; then
+        rm -rf /home/ubuntu/app-backup
     fi
     cd /
     mv /home/ubuntu/app /home/ubuntu/app-backup
