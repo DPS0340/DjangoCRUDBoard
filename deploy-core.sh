b0b6da478d75f77df7cd521dfb2a0f2e8d8b8f4b
jiho lee <optional.int@kakao.com>
2021-03-12 20:40:20 +0900

    Create deploy-core.sh

---
+++ deploy-core.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/bash
+REPOSITORY=$(dirname `which $0`)
+chown $(whoami) $REPOSITORY
+cd $REPOSITORY
+echo $Django_secret_key
+
+sudo apt-get update \
+  && sudo DEBIAN_FRONTEND=noninteractive apt-get install -y docker-compose
+
+chmod +x ./init-letsencrypt.sh
+
+./init-letsencrypt.sh -n
+
+mkdir -p ./data/db
+chmod -R 777 ./data
+touch ./nohup.out
+docker-compose down -v
+nohup docker-compose up --build > ./nohup.out 2>&1 &
