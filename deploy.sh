428154a830ab585603cf054c98536df9fd62eb6b
jiho lee <optional.int@kakao.com>
2021-03-12 20:39:44 +0900

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,29 +1,8 @@
 #!/bin/bash
 . /home/ubuntu/.bashrc
-
 export COMPOSE_PROJECT_NAME=DjangoCrudBoard
-
 REPOSITORY=$(dirname `which $0`)
-
 chown $(whoami) $REPOSITORY
-
 cd $REPOSITORY
-
-sudo apt-get update \
-  && sudo DEBIAN_FRONTEND=noninteractive apt-get install -y docker-compose
-
-chmod +x ./init-letsencrypt.sh
-
-./init-letsencrypt.sh -n
-
-
-mkdir -p ./data/db
-
-chmod -R 777 ./data
-
-chown -R ubuntu ./data
-
-touch ./nohup.out
-
-docker-compose down -v
-nohup docker-compose up --build > ./nohup.out 2>&1 &
+chmod +x ./deploy-core.sh
+./deploy-core.sh
